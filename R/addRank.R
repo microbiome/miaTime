1c630dff8a43ccda60315e5a926480c599daa7f6
YagmurSimsekk <yagmur.simsek.98@gmail.com>
2021-09-14 15:06:44 +0300

    addRank uncompleted

--- R/addRank.R
+++ R/addRank.R
@@ -1,17 +1,48 @@
-setGeneric("addRank", signature = c("x"),
-           function(x,
-                    method = c("average", "first", "last", "random", "max", "min"))
-             standardGeneric("addRank"))
+#' Rank the sample information in a `SummarizedExperiment` Object
+#'
+#' The information stored in `colData` in \linkS4class{SummarizedExperiment}
+#' object can be ranked by `addRank` function.
+#'
+#' @param x \linkS4class{SummarizedExperiment} object
+#' @param ... Allow new parameters to be defined for this function.
+#' @param na.last  NA values are put last when \code{TRUE} or put first when
+#' \code{FALSE} or they are kept with rank NA when \code{"keep"}
+#' @param ties.method a character string indicating the method used in ranking
+#'
+#' @return a list shows the ranked components of
+#' \linkS4class{SummarizedExperiment} object
+#'
+#' @examples
+#' library(SummarizedExperiment)
+#' data(airway, package="airway")
+#' se <- airway
+#'
+#' addRank(se, na.last ="TRUE", ties.method = "last")
+#'
+#' @importFrom SummarizedExperiment SummarizedExperiment
+#'
+#' @export
+setGeneric("addRank", signature = "x",
+        function(x,...)
+            standardGeneric("addRank"))
 
-setMethod("addRank", signature = c(x = "SummarizedExperiment"),
-          function(x,
-                   method = c("average", "first", "last", "random", "max", "min")){
-          col_data <- colData(x)
-          if (!is.vector(col_data)){
-            col_data <- as.vector(col_data)
-          }
+.is_integer <- function(t){
+    i <- seq_along(t)
+    for (i in t){
+        if(is.integer(t[[i]]) == FALSE){
+        t[-i]
+        }
+    }
+}
 
-          ranked <- rank(col_data, na.last = TRUE , method)
-          return(ranked)
-          }
-)
+setMethod("addRank", signature(x = "SummarizedExperiment"),
+            function(x,
+                    na.last = "TRUE",
+                    ties.method = c("average", "first", "last", "random", "max",
+                        "min"), ...){
+            col_data <- colData(x)
+            list <- col_data@listData
+            integerlist <- .is_integer(list) #lapply(list, .is_integer)
+            ranked <- rank(integerlist, na.last, ties.method)
+            return(ranked)
+    })
