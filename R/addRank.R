2922d124d79b40234ac0a2b097cabceb95b7fc04
YagmurSimsekk <yagmur.simsek.98@gmail.com>
2021-09-15 21:26:37 +0300

    updated

--- R/addRank.R
+++ R/addRank.R
@@ -5,41 +5,48 @@
 #' to \linkS4class{SummarizedExperiment} object.
 #'
 #' @param x \linkS4class{SummarizedExperiment} object
-#' @param field A character string indicating the field aimed to be ranked
-#' @param rank_field A single character value to name the newly ranked field
-#' @param na.last Logical scalar: NA values are put last when \code{TRUE} ,first
-#' when \code{FALSE}. If \code{'NA'}, they are removed; if \code{"keep"}
-#' they are kept with rank NA.(default: \code{norm = TRUE})
-#' @param ties.method a character string indicating the method used in ranking
+#' @param field A character value indicating the name of the matrix column that
+#' is aimed to be ranked.
+#' @param field_matrix A matrix that is aimed to be ranked. If the aimed matrix
+#' string already exists in colData segment, this optional matrix doesn't need
+#' to be added.
+#' @param rank_field_name A character value to name the newly ranked `field`
 #' @param ... Allow new parameters to be defined for this function.
 #'
-#' @return \linkS4class{SummarizedExperiment} object with ranked field added
+#' @return \linkS4class{RangedSummarizedExperiment} object with ranked field
+#' added
 #'
 #' @examples
 #' library(SummarizedExperiment)
 #' data(airway, package="airway")
 #' se <- airway
 #'
-#' addRank(se, field = "SampleName", rank_field = "SampleName_rank",
+#' abc <- addRank(se, field = "SampleName", rank_field_name = "SampleName_rank",
 #'                                 na.last ="TRUE", ties.method = "first")
 #'
 #' @importFrom SummarizedExperiment colData
+#' @importFrom SummarizedExperiment colData<-
 #' @importFrom methods setGeneric
 #'
 #' @export
 setGeneric("addRank", signature = "x",
-        function(x, field, rank_field, na.last, ties.method, ...)
+        function(x, field, field_matrix, rank_field_name, ...)
             standardGeneric("addRank"))
 
 setMethod("addRank", signature(x = "SummarizedExperiment"),
-            function(x, field, rank_field,
-                    na.last = TRUE,
-                    ties.method = c("average", "first", "last", "random",
-                                                        "max", "min"), ...){
+            function(x, field, field_matrix, rank_field_name, ...){
             col_data <- colData(x)
             mdat <- do.call(cbind,lapply(col_data, as.vector))
-            data <- mdat[,field]
-            ranked <- rank(data, na.last, ties.method)
-            x$rank_field <- ranked
+
+            if ( field %in% colnames(mdat) == TRUE){
+                data <- mdat[, field]
+                }  else {
+                        data <- field_matrix
+                        col_data[, field] <- data
+            }
+
+            ranked <- rank(data, ...)
+            col_data[, rank_field_name] <- ranked
+            colData(x) <- col_data
             return(x)
     })
