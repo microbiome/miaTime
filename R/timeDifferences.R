8694400d21249664f2cc389f7755b544c3725454
YagmurSimsekk <yagmur.simsek@hsrw.org>
2021-11-16 23:43:14 +0100

    conversion functions

--- R/timeDifferences.R
+++ R/timeDifferences.R
@@ -1,58 +1,35 @@
-#' Time field is provided in the format of "%H:%M:%S" can be converted to
-#' numeric values depending on the units. Consecutive time difference of these
-#' numeric values can be then calculated as well as the time difference based on
-#' the certain time point with parameter `baseline`.
+#' Consecutive time difference of numeric values can be calculated
+#' as well as the time difference based on the certain time point with
+#' parameter `baseline`.
 #'
-#' @param time_field : character vector, contains the time points
+#' @param time_field : vector containing time points
 #' @param baseline : integer, value to calculate the distance from certain time
 #' point. (default: \code{baseline = 1})
-#' @param units : The unit of time points selected in `time_field`.
-#' It can be hours, minutes or seconds.(default: \code{unit = "mins"})
 #'
 #' @examples
 #' data(hitchip1006)
-#' hitchipDate <- timeToDate(hitchip1006, time_field = "time", unit = "mins")
 #'
-#' hitchipTime <- timeDifferences(hitchipDate, baseline = 1, units = "mins")
+#' diffHitchip <- timeDifferences(time_field = colData(hitchip1006)[,"time"],
+#'                                              baseline = 1)
 #'
-#' ##Each of these fields in the list object can be applied to the dataset as
-#' #new fields.
+#' ##Each of these fields in the list object can be applied to the
+#' #\linkS4class{SummarizedExperiment} object as new fields.
 #'
-#' colData(hitchip1006)$time_diff <- hitchipTime$Consecutive
-#' colData(hitchip1006)$base_diff <- hitchipTime$Baseline
+#' colData(hitchip1006)$time_diff <- diffHitchip$Consecutive
+#' colData(hitchip1006)$base_diff <- diffHitchip$Baseline
 #'
-#' @return a list object containing the numeric values of time, consecutive time
-#' differences and the distance from the chosen time point to each time point.
+#' @return a list object containing consecutive time differences and
+#' the distance from selected time point to each time point.
 #'
 #' @export
-timeDifferences <- function(time_field, baseline = 1, units = "mins"){
-  hours <- substr(time_field, start = 1, stop = 2)
-  minutes <- substr(time_field, start = 4, stop = 5)
-  seconds <- substr(time_field, start = 7, stop = 8)
+timeDifferences <- function(time_field, baseline = 1){
 
-  num_h <- as.numeric(hours)
-  num_m <- as.numeric(minutes)
-  num_s <- as.numeric(seconds)
+    diff_time <- diff(time_field)
+    diff_time <- c(NA, diff_time)
+    base <- time_field[baseline]
+    i <- seq_len(length(time_field))
+    ifelse(time_field[i], base_diff <- time_field[i] - base, base_diff )
 
-  if (units == "hours"){
-    time <- num_h + num_m/60 + num_s/3600
-
-  } else if (units == "mins"){
-    time <- num_h * 60 + num_m + num_s/60
-
-  } else if (units == "secs"){
-    time<- num_h * 3600 + num_m * 60 + num_s
-
-  } else {
-    warning('There is no valid time unit input value is given')
-  }
-
-  diff_time <- diff(time)
-  diff_time <- c(NA, diff_time)
-  base <- time[baseline]
-  i <- seq_len(length(time))
-  ifelse(time[i], base_diff <- time[i] - base, base_diff )
-
-  return(list(timeNumeric = time, Consecutive = diff_time,
-              Baseline = base_diff))
+    return(list(Consecutive = diff_time,
+                Baseline = base_diff))
 }
--- R/timeToDate.R
+++ R/timeToDate.R
@@ -1,48 +1,81 @@
-#' Numerically provided time points are converted to a character object in the
-#' format "%H:%M:%S".
+#' Numerically provided time points are converted to a Period class object in
+#' "H M S" format based on unit. In addition to the time information,
+#' date information can also be given.
 #'
-#' @param data : dataset with numeric time points in it
-#' @param time_field : character, time points field in the dataset
+#' @param time_field : vector containing numeric time points
 #' @param unit : character, indicating the unit of given times
 #' (default: \code{unit = "mins"})
+#' @param date : vector containing date information in "YYYY-MM-DD" format.
+#' This information is optional.
 #'
 #' @examples
+#' library(miaTime)
 #' data(hitchip1006)
-#' hitchipDate <- timeToDate(hitchip1006, time_field = "time", unit = "mins")
+#'
+#' ##without date information
+#'
+#' hitchipTime <- timeToDate(time_field = colData(hitchip1006)[,"time"], unit = "mins")
+#'
+#' ##with date
+#'
+#' DateColumn <- seq.Date(as.Date("2011-05-01"), by = "days", length.out = 1151)
+#'
+#' hitchipDate <- timeToDate(time_field = colData(hitchip1006)[,"time"] , unit = "mins",
+#'                             date = DateColumn)
 #'
 #' ##This can be added back to \linkS4class{SummarizedExperiment} object that
 #' #is used to store `hitchip1006` dataset.
 #'
-#' colData(hitchip1006)$clock <- hitchipDate
+#' colData(hitchip1006)$clock <- hitchipTime
 #'
-#' @return a list object containing time in "%H:%M:%S" format.
+#' @importFrom SummarizedExperiment colData<-
+#' @importFrom lubridate hms
+#'
+#' @return a Period class object("H M S") or a list containing the date(s)
+#' information provided and a Period class object containing time information
 #'
 #' @export
-timeToDate<- function(data, time_field, unit = "mins"){
-    origin <- "1970-01-01"
-    time <- colData(data)[,time_field]
+timeToDate<- function(time_field, unit = "mins", date){
+
+    if (missing(date)){
+      origin <- rep("1970-01-01", length(time_field))
+    } else {
+      origin <- as.Date(date)
+    }
     if (unit == "mins"){
       hours <- 0
-      minutes <- floor(time)
-      seconds <- time - floor(time)
+      minutes <- floor(time_field)
+      seconds <- time_field - floor(time_field)
       seconds <- round(seconds, 2)*10
     } else if (unit == "hours"){
-      hours <- floor(time)
-      minutes <- time - floor(time)
+      hours <- floor(time_field)
+      minutes <- time_field - floor(time_field)
       minutes <- round(minutes, 2)*10
       seconds <- 0
+    } else if (unit == "secs"){
+      hours <- 0
+      minutes <- 0
+      seconds <- floor(time_field)
     }
 
     if (unit == "mins"){
         ct <- as.POSIXct(60 * minutes, tz = "UTC", origin) + seconds
     } else if (unit == "hours"){
         ct <- as.POSIXct(3600 * hours, tz = "UTC", origin) + 60*minutes
+    } else if (unit == "secs"){
+        ct <- as.POSIXct(seconds, tz = "UTC", origin)
     } else {
-        warning('The units must be either hours or minutes.
+        warning('The units must be hour(hours), minutes(mins), seconds(secs).
             The default value: mins is used')
     }
 
-    colData(data)$rank <- rank(colData(data)[,time_field])
     clock <- format(ct, format="%H:%M:%S")
-    return(clock)
+    period <- hms(clock)
+
+    if (missing(date)){
+      return(time = period)
+    } else {
+      return(list(time = period, date = origin))
+    }
+
 }
