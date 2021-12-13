fea370b793a742974eb75e0252f2424046009bb7
YagmurSimsekk <yagmur.simsek@hsrw.org>
2021-12-13 11:12:15 +0100

    function changed

--- R/getTimeDivergence.R
+++ R/getTimeDivergence.R
@@ -1,54 +1,78 @@
-#' Time difference between Nth time steps
+#' Beta diversity calculation between samples at the interval of n time steps
 #'
-#' The time difference can be calculated between successive time steps, with
-#' steps n >= 1.
+#' The dissimilarity (beta distance) is calculated in steps of n for each
+#' individual. "n" can be 1 or greater than 1. The time difference between
+#' the time points used for beta diversity calculation is also given as output.
 #'
 #' @param se A
 #' \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}}
-#' object.
+#' object or
+#' \code{\link[TreeSummarizedExperiment:TreeSummarizedExperiment-class]{TreeSummarizedExperiment}}
+#' @param sample_field column vector containing the all individuals in
+#' `colData` field
+#' @param sample_id character indicating a individual inside of `sample_field`
 #' @param time_field column vector containing the time information
-#' in `colData` field inside of \linkS4class{SummarizedExperiment} object
-#' @param starting_position integer value that defines from which time step the
-#' operation begins (default: \code{starting_position = 1})
+#' in `colData` field
 #' @param time_interval integer value indicating the increment between the time
 #' steps. It can be 1 or higher than 1.
+#' @param transposed logical scalar assigning samples to rows if they are in
+#' columns (default: \code{transposed = FALSE})
 #'
-#' @return `colData` field in \linkS4class{SummarizedExperiment} object
+#' @return a list containing the beta diversity in matrix and a numeric vector
+#' containing the time difference between the samples
 #'
 #' @importFrom SummarizedExperiment colData
+#' @importFrom vegan vegdist
 #'
 #' @examples
-#' library(miaTime)
-#' data(hitchip1006)
-#' se <- hitchip1006
+#' library(microbiomeDataSets)
+#' tse <- GrieneisenTSData()
 #'
-#' seventeenTime <- getTimeDivergence(se, time_field = "time" , time_interval = 17)
+#' BaboonDivergence <- getTimeDivergence(tse, sample_field = "baboon_id",
+#'                                     sample_id = "Baboon_1" ,
+#'                                     time_field = "collection_date",
+#'                                     time_interval =2,
+#'                                     transposed = FALSE )
 #'
 #'@export
-getTimeDivergence <- function(se, time_field, starting_position = 1, time_interval){
+getTimeDivergence <- function(se, sample_field, sample_id, time_field, time_interval, transposed = FALSE ){
 
-    n <- 1:length(colData(se)[, time_field])
+    df <-as.data.frame(colData(se)[, c(sample_field, time_field)])
 
-    if (!(time_interval %in% n)){
-        stop("Time interval cannot be bigger than the length of time series")
+    #per sample
+    sample_df <- df[which((df[, sample_field] == sample_id) == TRUE),]
+
+    mat <- data.matrix(sample_df)
+
+    #time in descending order
+    mat[, time_field] <- mat[, time_field][order(mat[, time_field], decreasing = FALSE)]
+
+    # Nth times are extracted
+    location <-seq(0, length(mat[, time_field]), time_interval)
+
+    extracted_time <- mat[, time_field][location]
+
+    mat[, time_field] <- matrix(NA, ncol = 1, nrow = length(time))
+
+    #extracted time places back
+    mat[, time_field][location] <- extracted_time
+
+    #related samples are chosen from assay
+    x <- as.matrix(assay(tse)[,rownames(sample_df)[which(is.na(mat[, time_field]) == FALSE)]])
+
+    if(!transposed){
+      x <- t(x)
     }
 
-    vector <- colData(se)[, time_field]
-    location <- seq(starting_position, length(vector), time_interval)
+    timedivergence_n <- as.matrix(vegan::vegdist(x), "bray")
 
-    x <- (vector)[location]
+    #timedivergence_n <-do.call(stats::dist, list(x))
+    #timedivergence_n <- as.data.frame(as.matrix(time_divergence_n))
 
-    time_diff <- c(NA, diff(x))
+    time_diff <- diff(extracted_time)
 
-    timedivergence_n <- matrix(NA, ncol = 1, nrow = length(n))
-    timedifference_n <- matrix(NA, ncol = 1, nrow = length(n))
+    timedifference_n <- c(NA, time_diff)
 
-    for (i in location){
-        for (j in seq_len(length(location))){
-            timedivergence_n[i] <- x[j]
-            timedifference_n[i] <- time_diff[j]
-        }
-    }
-    df <- as.data.frame(cbind(timedivergence_n, timedifference_n))
-  return(df)
+    return(list(timedivergence_n = timedivergence_n,
+                timedifference_n = timedifference_n))
 }
