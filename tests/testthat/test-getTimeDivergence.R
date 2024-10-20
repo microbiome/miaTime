aa5470c56be6aada80a429edb7369c4d03cafe93
TuomasBorman <tvborm@utu.fi>
2024-10-20 13:40:30 +0300

    up

--- tests/testthat/test-getTimeDivergence.R
+++ tests/testthat/test-getTimeDivergence.R
@@ -1,137 +1,138 @@
 test_that("getStepwiseDivergence", {
-  data(hitchip1006)
-  tse <- hitchip1006
-  # Subset to speed up computing
-  # Just pick 4 subjects with 1-5 time points
-  tse <- tse[, colData(tse)$subject %in% c("900", "934", "843", "875")]
-  tse2 <- addStepwiseDivergence(
-    tse, group = "subject", time.interval = 1, time.col = "time",
-    assay.type="counts", dis.fun = vegan::vegdist, method = "bray",
-    name.time = "time_difference")
-
-  # Trying to add new coldata field with the same name
-  expect_warning(tse2 <- addStepwiseDivergence(
-    tse2, group = "subject", time.interval = 1, time.col = "time",
-    name.time = "time_difference"))
-
-  # Input and output classes should match
-  expect_equal(class(tse), class(tse2))
-
-  # A subject to check time difference calculation
-  obs_diff <- colData(tse2)[
-      which(colData(tse2)[, "subject"] == "843"), "time_difference"]
-  exp_diff <- c(NA,diff(colData(tse)[
-      which(colData(tse)[, "subject"] == "843"), "time"]))
-  expect_equal(obs_diff, exp_diff)
-
-  # n > 1
-  tse3 <- addStepwiseDivergence(
-    tse, group = "subject", time.interval = 2, time.col = "time",
-    assay.type = "counts", dis.fun = vegan::vegdist, method = "bray",
-    name.time = "time_difference")
-  
-  time_invertal <- 2
-
-  time3 <- colData(tse3)[, "time"][which(colData(tse3)[, "subject"] == "843")]
-
-  time_dif_3 <- colData(tse3)[, "time_difference"][
-      which(colData(tse3)[, "subject"] == "843")]
-
-  # number of divergences (n-k) check
-  divergence_number <- length(time3) - time_invertal
-
-  divergence_calculated <- length(which(!is.na(time_dif_3) == TRUE))
-
-  expect_equal(divergence_number, divergence_calculated)
-
-  # interval check
-  calculated_diff <- time3[(1+ 2):length(time3)] - 
-      time3[seq_len(length(time3)-2)]
-
-  manual_diff <- c(rep(NA, length(time3) - 
-      length(calculated_diff)), calculated_diff)
-
-  expect_equal(time_dif_3, manual_diff)
-
-  # object with single time point has NA instead of divergence values
-  sub_hitchip <- hitchip1006[, 
-      colData(hitchip1006)$subject %in% c("900","843", "139")]
-  subset <- addStepwiseDivergence(
-    sub_hitchip, group = "subject", time.interval = 1, time.col = "time",
-    assay.type = "counts", dis.fun = vegan::vegdist, method = "bray",
-    name = "time_divergence",
-    name.time = "time_difference")
-
-  expect_true(all(is.na(colData(subset)[, "time_divergence"][
-      which(duplicated(colData(subset)[, "subject"]) == FALSE)])))
-
-
-  # Test vegan distances
-  tse2 <- addStepwiseDivergence(
-    tse, group = "subject", time.interval = 1, time.col = "time",
-    assay.type = "counts", dis.fun = vegan::vegdist, method = "bray",
-    name.time = "timedifference", name = "timedivergence")
-  
-  # Test vegan distances
-  tse2 <- addStepwiseDivergence(
-    tse2, group = "subject", time.interval = 1, time.col = "time",
-    assay.type="counts", dis.fun = vegan::vegdist, method = "euclidean",
-    name.time = "timedifference2", name = "timedivergence2")
-				     
-  # Time differences should still match
-  expect_true(identical(tse2$timedifference, tse2$timedifference2))
-  # ... but divergences should be different (bray vs. euclid)
-  expect_true(!identical(tse2$timedivergence, tse2$timedivergence2))
-  
-  # ## Test with ordination values
-  # tse2 <- scater::runMDS(tse2, FUN = vegan::vegdist, method = "bray",
-  #                       name = "PCoA_BC", exprs_values = "counts",
-  #                       na.rm = TRUE, ncomponents=4)
-  # # testing with all ordination components; ndimred=NULL --> all 4 components
-  # tse2 <- addStepwiseDivergence(tse2, group = "subject",
-  #                               time.interval = 1,
-  #                               time.col = "time",
-  #                               name.time="timedifference_ord_4",
-  #                               name="timedivergence_ord_4",
-  #                               dimred = "PCoA_BC",
-  #                               dis.fun=vegan::vegdist,
-  #                               method="euclidean")
-  # # Time differences should still match
-  # expect_true(identical(tse2$timedifference_ord_4, tse2$timedifference))
-  # # ordination based divergence values should not be equal to the ones on counts
-  # expect_true(!identical(tse2$timedivergence_ord_4, tse2$timedivergence))
-  # 
-  # # testing with 2 ordination components
-  # tse2 <- addStepwiseDivergence(tse2, group = "subject",
-  #                               time.interval = 1,
-  #                               time.col = "time",
-  #                               name.time="timedifference_ord_2",
-  #                               name="timedivergence_ord_2",
-  #                               dimred = "PCoA_BC",
-  #                               ndimred = 2,
-  #                               dis.fun=vegan::vegdist,
-  #                               method="euclidean")
-  # # Time differences should still match
-  # expect_true(identical(tse2$timedifference_ord_2, tse2$timedifference_ord_4))
-  # not same values as using 4 components
-  #expect_true(!identical(tse2$timedivergence_ord_2, tse2$timedivergence_ord_4))
-  
-  ## testing with altExp
-  tse <- hitchip1006
-  altExp(tse, "Family") <- agglomerateByRank(tse,  rank = "Family")
-  tse <- addStepwiseDivergence(
-    tse, group = "subject", time.interval = 1, time.col = "time",
-    altexp = "Family", name.time = "timedifference_Fam",
-    name = "timedivergence_Fam")
-  altExp(tse, "Family") <- addStepwiseDivergence(
-    altExp(tse, "Family"), group = "subject", time.interval = 1,
-    time.col = "time", name.time = "timedifference",
-    name = "timedivergence")
-  # Time differences should still match
-  expect_equal(
-    tse$timedivergence_Fam,
-    altExp(tse, "Family")$timedifference)
-  # divergence values based on Family rank counts should not be equal to the
-  # ones with Genus counts
-  #expect_true(!identical(tse2$timedivergence_Fam, tse2$timedivergence))
+    data(hitchip1006)
+    tse <- hitchip1006
+    # Subset to speed up computing
+    # Just pick 4 subjects with 1-5 time points
+    tse <- tse[, colData(tse)$subject %in% c("900", "934", "843", "875")]
+    tse2 <- addStepwiseDivergence(
+        tse, group = "subject", time.interval = 1, time.col = "time",
+        assay.type="counts", dis.fun = vegan::vegdist, method = "bray",
+        name.time = "time_difference")
+    
+    # Trying to add new coldata field with the same name
+    expect_warning(tse2 <- addStepwiseDivergence(
+        tse2, group = "subject", time.interval = 1, time.col = "time",
+        name.time = "time_difference"))
+    
+    # Input and output classes should match
+    expect_equal(class(tse), class(tse2))
+    
+    # A subject to check time difference calculation
+    obs_diff <- colData(tse2)[
+        which(colData(tse2)[, "subject"] == "843"), "time_difference"]
+    exp_diff <- c(NA,diff(colData(tse)[
+        which(colData(tse)[, "subject"] == "843"), "time"]))
+    expect_equal(obs_diff, exp_diff)
+    
+    # n > 1
+    tse3 <- addStepwiseDivergence(
+        tse, group = "subject", time.interval = 2, time.col = "time",
+        assay.type = "counts", dis.fun = vegan::vegdist, method = "bray",
+        name.time = "time_difference")
+    
+    time_invertal <- 2
+    
+    time3 <- colData(tse3)[, "time"][which(colData(tse3)[, "subject"] == "843")]
+    
+    time_dif_3 <- colData(tse3)[, "time_difference"][
+        which(colData(tse3)[, "subject"] == "843")]
+    
+    # number of divergences (n-k) check
+    divergence_number <- length(time3) - time_invertal
+    
+    divergence_calculated <- length(which(!is.na(time_dif_3) == TRUE))
+    
+    expect_equal(divergence_number, divergence_calculated)
+    
+    # interval check
+    calculated_diff <- time3[(1+ 2):length(time3)] - 
+        time3[seq_len(length(time3)-2)]
+    
+    manual_diff <- c(rep(NA, length(time3) - 
+        length(calculated_diff)), calculated_diff)
+    
+    expect_equal(time_dif_3, manual_diff)
+    
+    # object with single time point has NA instead of divergence values
+    sub_hitchip <- hitchip1006[, 
+        colData(hitchip1006)$subject %in% c("900","843", "139")]
+    subset <- addStepwiseDivergence(
+        sub_hitchip, group = "subject", time.interval = 1, time.col = "time",
+        assay.type = "counts", dis.fun = vegan::vegdist, method = "bray",
+        name = "time_divergence",
+        name.time = "time_difference")
+    
+    expect_true(all(is.na(
+        colData(subset)[, "time_divergence"][
+            which(duplicated(colData(subset)[, "subject"]) == FALSE)])))
+    
+    
+    # Test vegan distances
+    tse2 <- addStepwiseDivergence(
+        tse, group = "subject", time.interval = 1, time.col = "time",
+        assay.type = "counts", dis.fun = vegan::vegdist, method = "bray",
+        name.time = "timedifference", name = "timedivergence")
+    
+    # Test vegan distances
+    tse2 <- addStepwiseDivergence(
+        tse2, group = "subject", time.interval = 1, time.col = "time",
+        assay.type="counts", dis.fun = vegan::vegdist, method = "euclidean",
+        name.time = "timedifference2", name = "timedivergence2")
+    			     
+    # Time differences should still match
+    expect_true(identical(tse2$timedifference, tse2$timedifference2))
+    # ... but divergences should be different (bray vs. euclid)
+    expect_true(!identical(tse2$timedivergence, tse2$timedivergence2))
+    
+    # ## Test with ordination values
+    # tse2 <- scater::runMDS(tse2, FUN = vegan::vegdist, method = "bray",
+    #                       name = "PCoA_BC", exprs_values = "counts",
+    #                       na.rm = TRUE, ncomponents=4)
+    # # testing with all ordination components; ndimred=NULL --> all 4 components
+    # tse2 <- addStepwiseDivergence(tse2, group = "subject",
+    #                               time.interval = 1,
+    #                               time.col = "time",
+    #                               name.time="timedifference_ord_4",
+    #                               name="timedivergence_ord_4",
+    #                               dimred = "PCoA_BC",
+    #                               dis.fun=vegan::vegdist,
+    #                               method="euclidean")
+    # # Time differences should still match
+    # expect_true(identical(tse2$timedifference_ord_4, tse2$timedifference))
+    # # ordination based divergence values should not be equal to the ones on counts
+    # expect_true(!identical(tse2$timedivergence_ord_4, tse2$timedivergence))
+    # 
+    # # testing with 2 ordination components
+    # tse2 <- addStepwiseDivergence(tse2, group = "subject",
+    #                               time.interval = 1,
+    #                               time.col = "time",
+    #                               name.time="timedifference_ord_2",
+    #                               name="timedivergence_ord_2",
+    #                               dimred = "PCoA_BC",
+    #                               ndimred = 2,
+    #                               dis.fun=vegan::vegdist,
+    #                               method="euclidean")
+    # # Time differences should still match
+    # expect_true(identical(tse2$timedifference_ord_2, tse2$timedifference_ord_4))
+    # not same values as using 4 components
+    #expect_true(!identical(tse2$timedivergence_ord_2, tse2$timedivergence_ord_4))
+    
+    ## testing with altExp
+    tse <- hitchip1006
+    altExp(tse, "Family") <- agglomerateByRank(tse,  rank = "Family")
+    tse <- addStepwiseDivergence(
+        tse, group = "subject", time.interval = 1, time.col = "time",
+        altexp = "Family", name.time = "timedifference_Fam",
+        name = "timedivergence_Fam")
+    altExp(tse, "Family") <- addStepwiseDivergence(
+        altExp(tse, "Family"), group = "subject", time.interval = 1,
+        time.col = "time", name.time = "timedifference",
+        name = "timedivergence")
+    # Time differences should still match
+    expect_equal(
+        tse$timedivergence_Fam,
+        altExp(tse, "Family")$timedifference)
+    # divergence values based on Family rank counts should not be equal to the
+    # ones with Genus counts
+    #expect_true(!identical(tse2$timedivergence_Fam, tse2$timedivergence))
 })
