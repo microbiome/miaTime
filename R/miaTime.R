216f1d1c49cee2b9ab9d32a713e3d3f55c7e582e
TuomasBorman <tvborm@utu.fi>
2025-04-02 17:36:00 +0300

    up

--- R/miaTime.R
+++ R/miaTime.R
@@ -138,16 +138,50 @@ NULL
 #' @keywords data
 #'
 NULL
-#' @title crohn_survival
-#' @description
+
+#' @title
 #' Survival microbiome data from 150 individuals with longitudinal measurements
-#' crohn_survival is a simulated dataset based on the Crohn's disease microbiome study,
-#' containing survival data with microbiome composition for 150 individuals measured
-#' at 150 time points. The data is designed for compositional analysis of microbiome
-#' format of the dataset: TreeSummarisedExperiment
-#' rows/features = 150 
-#' The row metadata of the microbiome data contains taxonomic information on
-#' the Kingdom, Phylum, Class, Order, Family and Genus and Species level.
-#' number of taxa= 48
-#' event: Binary variable indicating disease status (1 = Crohn’s Disease, 0 = Control).
-#' reference: DOI 10.5281/zenodo.10552383
+#'
+#' @description
+#' Simulated dataset based on a Crohn's disease microbiome study. The dataset
+#' is right-censored and includes time-to-event data, representing either the
+#' occurrence of disease or censoring. It contains 150 individuals and 48 taxa.
+#' The dataset originates from the \pkg{coda4microbiome} package under the
+#' name "data_survival".
+#'
+#' @details
+#' Sample metadata includes the following information:
+#'  - diagnosis: Indicates whether an individual has Crohn’s disease (CD) or
+#'    is a control.
+#'  - event: Binary variable indicating disease status (1 = Crohn’s Disease,
+#'    0 = Control).
+#'  - event_time: The time of event occurrence or censoring (unitless time).
+#'
+#' Taxa data do not include additional taxonomy information.
+#'
+#' @references
+#' Calle ML et al. (2023) coda4microbiome: compositional data analysis for
+#' microbiome cross-sectional and longitudinal studies. BMC Bioinformatics,
+#' 24(82). \url{https://doi.org/10.1186/s12859-023-05205-3}
+#'
+#' Gevers D et al. (2018) The Treatment-Naive Microbiome in New-Onset Crohn’s
+#' Disease. Cell Host & Microbe, 15(4).
+#' #' \url{https://doi.org/10.1016/j.chom.2014.02.005}
+#'
+#' @docType
+#' data
+#'
+#' @return
+#' Loads the dataset in R.
+#'
+#' @usage
+#' data(crohn_survival)
+#'
+#' @format
+#' The dataset is in the
+#' \code{\link[TreeSummarizedExperiment:TreeSummarizedExperiment-class]{TreeSummarizedExperiment}}
+#' format.
+#'
+#' @keywords
+#' data
+NULL
--- inst/scripts/crohn_survival.R
+++ inst/scripts/crohn_survival.R
@@ -2,41 +2,44 @@
 library(TreeSummarizedExperiment)
 library(S4Vectors)
 
+# Get data from coda4microbiome package
 data("data_survival", package = "coda4microbiome")
 crohn_survival <- x
 assay_matrix <- as.matrix(crohn_survival)
 assay_matrix <- t(assay_matrix)
 taxa_names <- rownames(assay_matrix)
 
+# Prepare rowData
 rd <- lapply(mia:::.taxonomy_rank_prefixes, function(rank){
-  tax <- rep(NA_character_, length(taxa_names))
-  prefix <- paste0("^", rank, "__")
-  found <- grepl(prefix, taxa_names)
-  tax[found] <- gsub(prefix, "", taxa_names[found])
-  return(tax)
+    tax <- rep(NA_character_, length(taxa_names))
+    prefix <- paste0("^", rank, "__")
+    found <- grepl(prefix, taxa_names)
+    tax[found] <- gsub(prefix, "", taxa_names[found])
+    return(tax)
 })
 rd <- do.call(cbind, rd) |> DataFrame()
 empty <- sapply(rd, function(x) all(is.na(x)))
 rd <- rd[, !empty]
 rd[["taxonomy_id"]] <- taxa_names
 
-event <- as.numeric(Event)  
-event_time <- as.numeric(Event_time) 
+# Prepare colData
+event <- as.numeric(Event)
+event_time <- as.numeric(Event_time)
 diagnosis <- ifelse(event == 1, "CD", "control")
 
-rowData <- DataFrame((taxa_names), row.names = taxa_names)
-colData <- DataFrame(
-  sample_id = colnames(assay_matrix),
-  event,
-  event_time,
-  diagnosis,
-  row.names = colnames(assay_matrix)
+cd <- DataFrame(
+    sample_id = colnames(assay_matrix),
+    event,
+    event_time,
+    diagnosis,
+    row.names = colnames(assay_matrix)
 )
 
+# Construct TreeSE
 crohn_survival <- TreeSummarizedExperiment(
      assays = SimpleList(counts = assay_matrix),
-     rowData = rowData,
-     colData = colData
+     rowData = rd,
+     colData = cd
    )
 
-save(crohn_survival, file = "~/miaTime/data/crohn_survival.rda")
+save(crohn_survival, file = "data/crohn_survival.rda")
