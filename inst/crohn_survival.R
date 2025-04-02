163de159148ccf24278618de1e24bbd3c89e3006
Ali <alihajj824@gmail.com>
2025-04-02 10:11:32 +0300

    script correction

--- inst/crohn_survival.R
+++ inst/crohn_survival.R
@@ -2,50 +2,36 @@
 library(TreeSummarizedExperiment)
 library(S4Vectors)
 
-
 data("data_survival", package = "coda4microbiome")
 crohn_survival <- x
 assay_matrix <- as.matrix(crohn_survival)
 assay_matrix <- t(assay_matrix)
 taxa_names <- rownames(assay_matrix)
-str(x)
-# Function to Parse Taxonomy Labels
-parse_taxonomy <- function(taxa_names) {
-  # Create the taxonomy data frame
-  taxonomy_df <- data.frame(TaxonID = taxa_names, stringsAsFactors = FALSE)
-  
-  # Add structured taxonomy placeholders
-  taxonomy_df$Kingdom <- NA_character_
-  taxonomy_df$Phylum <- NA_character_
-  taxonomy_df$Class <- NA_character_
-  taxonomy_df$Order <- NA_character_
-  taxonomy_df$Family <- NA_character_
-  taxonomy_df$Genus <- NA_character_
-  taxonomy_df$Species <- NA_character_
-  
-  # Return the modified taxonomy dataframe
-  return(taxonomy_df)
-}
 
+rd <- lapply(mia:::.taxonomy_rank_prefixes, function(rank){
+  tax <- rep(NA_character_, length(taxa_names))
+  prefix <- paste0("^", rank, "__")
+  found <- grepl(prefix, taxa_names)
+  tax[found] <- gsub(prefix, "", taxa_names[found])
+  return(tax)
+})
+rd <- do.call(cbind, rd) |> DataFrame()
+empty <- sapply(rd, function(x) all(is.na(x)))
+rd <- rd[, !empty]
+rd[["taxonomy_id"]] <- taxa_names
 
-# Fill known taxonomic ranks
+event <- as.numeric(Event)  
+event_time <- as.numeric(Event_time) 
+diagnosis <- ifelse(event == 1, "CD", "control")
 
-
-# Create an example taxonomy dataframe with columns for Genus, Family, and Order
-taxonomy_df <- data.frame(Genus = rep(NA, length(taxa_names)),
-                          Family = rep(NA, length(taxa_names)),
-                          Order = rep(NA, length(taxa_names)),
-                          stringsAsFactors = FALSE)
-
-# Now your loop should work as expected
-taxonomy_df$Genus[grepl("^g__", taxa_names)] <- sub("^g__", "", taxa_names[grepl("^g__", taxa_names)])
-taxonomy_df$Family[grepl("^f__", taxa_names)] <- sub("^f__", "", taxa_names[grepl("^f__", taxa_names)])
-
-Event <- as.numeric(Event)  
-Event_time <- as.numeric(Event_time) 
-
-rowData <- DataFrame(parse_taxonomy(taxa_names), row.names = taxa_names)
-colData <- DataFrame(SampleID = colnames(assay_matrix), Event, Event_time, row.names = colnames(assay_matrix))
+rowData <- DataFrame((taxa_names), row.names = taxa_names)
+colData <- DataFrame(
+  sample_id = colnames(assay_matrix),
+  event,
+  event_time,
+  diagnosis,
+  row.names = colnames(assay_matrix)
+)
 
 crohn_survival <- TreeSummarizedExperiment(
      assays = SimpleList(counts = assay_matrix),
@@ -53,12 +39,4 @@ crohn_survival <- TreeSummarizedExperiment(
      colData = colData
    )
 
-
-save(crohn_survival, file = "data/crohn_survival.rda")
-
-
-
-
-
-
-
+save(crohn_survival, file = "~/miaTime/data/crohn_survival.rda")
